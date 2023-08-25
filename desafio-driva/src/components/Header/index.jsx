506fee136a217a9b05084cc8df71aa78d0570413
Lucas Souza <lucassouzafranco1@gmail.com>
2023-08-25 10:14:21 -0300

    ✨ feat(header): component creation and styling
    remove unused files, reset formatting and styled-components lib

---
+++ desafio-driva/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { 
+  HeaderArea, HeaderTitle, 
+} from './styleHeader';
+
+function Header() {
+  return (
+    <HeaderArea>
+      <HeaderTitle>Desafio Driva</HeaderTitle>
+    </HeaderArea>
+  )
+}
+
+export default Header;
---
+++ desafio-driva/src/components/Header/styleHeader.js
@@ -0,0 +1,15 @@
+import { styled } from "styled-components";
+
+export const HeaderArea = styled.div`
+  width: 100vw;
+  height: 7vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #282828;
+`
+
+export const HeaderTitle = styled.h2`
+  font-weight: 500;
+  background-color: #282828;
+`
