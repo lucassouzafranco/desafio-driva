f1e221d9fee42411b33bab629d6f739d1b48a690
Lucas Souza <lucassouzafranco1@gmail.com>
2023-09-01 10:39:50 -0300

    ✨ feat(company card): get companies data from HTTP request and render in cards

--- desafio-driva/src/components/CompanyList/index.jsx
+++ desafio-driva/src/components/CompanyList/index.jsx
@@ -1,23 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   ContainerCentralizer,
   ListContainer,
-  Title, 
+  Title,
   List,
   ListCentralizer,
   Company,
   Name
 } from './styleCompanyList';
+import axios from 'axios';
 
 const CompanyList = () => {
+
+  const [companies, setCompanies] = useState([]);
+  const [click, setClick] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/companies');
+        setCompanies(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  function handleCompanyButton() {
+
+  }
+
   return (
     <ContainerCentralizer>
       <ListContainer>
         <Title>Lista de empresas</Title>
         <ListCentralizer>
-            <List>
-              <Company><Name>Name</Name></Company>
-            </List>
+          <List>
+            {companies.map((company, index) => (
+              <Company 
+              key={company.id}
+              imagePath={company.image_path}
+              alt={company.name}
+              onClick={handleCompanyButton}
+              >
+                <Name>{company.name}</Name>
+              </Company>
+            ))}
+          </List>
         </ListCentralizer>
       </ListContainer>
     </ContainerCentralizer>
--- desafio-driva/src/components/CompanyList/styleCompanyList.js
+++ desafio-driva/src/components/CompanyList/styleCompanyList.js
@@ -4,48 +4,64 @@ export const ContainerCentralizer = styled.div`
   height: 86vh;
   display: flex;
   justify-content: center;
-`
+`;
 
 export const ListContainer = styled.div`
-  width: 50%;
   border-radius: 9px;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-`
+  `;
 
 export const Title = styled.h2`
   text-align: center;
   font-weight: 300;
-  margin-top: 0.5em;
-`
+  margin-top: 1.5em;
+  `;
 
 export const ListCentralizer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 90%;
-`
+  height: 75%;
+  `;
 
 export const List = styled.div`
-  height: 90%;
-  width: 92%;
-  display: inline-flex;
+  height: 85%;
+  display: flex;
+  flex-wrap: wrap;
   border: 1px solid #262626;
   border-radius: 5px;
-`
+  justify-content: center;
+  align-items: center;
+  padding: 1.5em 0;
+  `;
 
-export const Company = styled.div `
-  width: 18%;
-  height: 25%;
+export const Company = styled.button`
+  position: relative;
+  width: 14%;
+  height: 37%;
   border-radius: 5px;
   box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 35px;
   margin: 0 1em;
   border: 1px solid #262626;
-  margin-top: 1.5em;
-  text-align: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  background-image: url(${(props) => props.imagePath});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-color: #121212;
+  cursor: pointer;
+  transition: transform 0.2s ease; /* Efeito de transição de aumento */
+
+  &:hover {
+    transform: scale(1.1); /* Aumenta o card em 10% ao passar o mouse */
+  }
+`;
 
 export const Name = styled.p`
-  margin-top: 9%;
-  font-size: 14px;
+  font-size: 13px;
   color: #929292;
-`
+  background-color: transparent;
+  padding: 0.2em 0.5em;
+`;
